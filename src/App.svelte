2652712d14b96708056e650d262e8bbdc2f7b8b1
Erik Hedenström <Erik.Hedenstrom@postkodlotteriet.se>
2019-12-04 16:18:55 +0100

    Working on item list

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,75 @@
 <script>
-  import { Chip } from 'smelte';
-  export let name;
-  let closed = false;
-  let clicked = false;
+  import { Dialog, TextField, Button } from 'smelte';
+  import Item from './Item.svelte';
+
+  let showDialog = false;
+  let newItemName = '';
+  let newItemMin = '1';
+  let newItemMax = '1';
+
+  let items = [];
+
+  const closeDialog = async () => {
+    showDialog = false;
+    newItemName = '';
+    newItemMin = '1';
+    newItemMax = '1';
+  };
+
+  const addItem = async () => {
+    const newItem = {
+      name: newItemName,
+      min: newItemMin,
+      max: newItemMax,
+      current: 0,
+      createdAt: new Date().toISOString()
+    };
+    items = items.concat([newItem]);
+    closeDialog();
+  };
+
+  const removeItem = async event => {
+    const { item } = event.detail;
+    items = items.filter(i => i !== item);
+  };
 </script>
 
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
+    max-width: 480px;
     margin: 0 auto;
+    border: 1px solid blue;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
+  @media (max-width: 479px) {
     main {
+      border: 1px solid red;
       max-width: none;
     }
   }
 </style>
 
+<Dialog bind:value={showDialog}>
+  <h5 slot="title">Add Item</h5>
+  <div>
+    <TextField label="Name" type="text" bind:value={newItemName} />
+    <TextField label="Minimum " type="number" bind:value={newItemMin} />
+    <TextField label="Maximum" type="number" bind:value={newItemMax} />
+  </div>
+  <div slot="actions">
+    <Button color="secondary" text on:click={closeDialog}>Cancel</Button>
+    <Button color="primary" text on:click={addItem}>OK</Button>
+  </div>
+</Dialog>
+
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-  <Chip
-    icon="face"
-    removable
-    selectable
-    on:close={() => (closed = true)}
-    on:click={() => (clicked = true)}>
-    {name}
-  </Chip>
+  <ul>
+    {#each items as item}
+      <Item {item} on:remove={removeItem} />
+    {/each}
+  </ul>
 </main>
+
+<div class="absolute bottom-0 right-0">
+  <Button icon="add" on:click={() => (showDialog = true)} />
+</div>
---
+++ src/Item.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import { Slider, Button, Chip } from 'smelte';
+
+  const dispatch = createEventDispatcher();
+
+  function remove() {
+    dispatch('remove', { item });
+  }
+
+  export let item;
+</script>
+
+<li class="flex flex-row bg-gray-200">
+  <span class="flex-grow">{item.name}</span>
+  <Button icon="remove" text light flat />
+  {item.current}
+  <Button icon="add" text light flat />
+  <Button icon="delete" text flat />
+</li>
